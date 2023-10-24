cf8f7b65a2e96da4b1ca9535ad287f1c6e13a897
Reynaldo <reynaldommaciel@gmail.com>
2023-10-24 15:06:49 -0300

    init project

---
+++ lib/screens/your_places.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class YourPlacesScreen extends StatelessWidget {
+  const YourPlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
